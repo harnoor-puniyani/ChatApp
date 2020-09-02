76bf4356460ee0cabf197dfdc0aa430a1f1cd848
Harnoor Puniyani <18042000harjee@gmail.com>
2020-09-03 01:27:19 +0530

    Only last stage of styling is left

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -6,11 +6,17 @@ const users = {}
 
 io.on('connection',socket =>{
     socket.on('new-user-joined' , name=>{
+        // console.log("New User",name);
         users[socket.id]=name;
         socket.broadcast.emit('user-joined',name);
-    })
+    });
 
     socket.on('send',message =>{
-        socket.broadcast.emit('receive',{message : message , name :user[socket.id]})
+        socket.broadcast.emit('receive',{message : message , name :users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
     })
-})
+}) 
