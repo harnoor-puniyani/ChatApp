068f33a961611f9f914c6c1eda32365c19025c55
Harnoor Puniyani <18042000harjee@gmail.com>
2020-09-04 20:30:05 +0530

    added comments

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -5,6 +5,7 @@ const io=require('socket.io')(8000)
 const users = {}
 
 io.on('connection',socket =>{
+    //Listening for new users to join!
     socket.on('new-user-joined' , name=>{
         // console.log("New User",name);
         users[socket.id]=name;
@@ -12,10 +13,12 @@ io.on('connection',socket =>{
     });
 
     socket.on('send',message =>{
+        // Event used for sending new messages to the chat group
         socket.broadcast.emit('receive',{message : message , name :users[socket.id]})
     });
 
     socket.on('disconnect', message =>{
+        // listening for connections or whenever someone leaves the chat window!
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     })
