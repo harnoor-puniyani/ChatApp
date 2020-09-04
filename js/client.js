d57ff3ae2ae9328b2225905f493e36f98c17b040
Harnoor Puniyani <18042000harjee@gmail.com>
2020-09-04 20:31:31 +0530

    Added comments and enhanced multi-level support

--- js/client.js
+++ js/client.js
@@ -1,11 +1,13 @@
+//connecting to the Nodeserver
 const socket =io('http://localhost:8000');
-
+//asssigning to the node variables for  controlability of it.
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp')
 const messageContainer = document.querySelector(".container")
-
+// applying notification audio!
 var audio = new Audio('juntos.mp3');
 
+// function which will append info to the container!
 const append = (message,position)=>{
     const messageElement = document.createElement('div');
     messageElement.innerText=message;
@@ -16,7 +18,7 @@ const append = (message,position)=>{
         audio.play();
     }
 }
-
+//this event helps submit the form or better say sending the messages
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInput.value;
@@ -25,19 +27,19 @@ form.addEventListener('submit', (e)=>{
     messageInput.value=''
 
 })
-
+// the prompt takes in the name of the users
 const name = prompt('Enter your Name to join');
 socket.emit('new-user-joined',name)
-
+// this will give a text-message whenever the user enters , wuthout notification
 socket.on('user-joined', name=>{
 append(`${name} joined the chat`,'right'); 
 }) 
-
+// the format of how to receive the text messages from other chat members
 socket.on('receive',data =>{
     append(`${data.name} : ${data.message}`,'left');
 }) 
-
+//the format of receiving text messages whenever someone leaves the chat.
 socket.on('left' , name=>{
-    append(`${name} left the chat`,'left');
+    append(`${name} left the chat`,'right');
 } )
 
